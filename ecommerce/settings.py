acec7c01a9f1d3a57146ecc678d4f0e258dc700f
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-02-24 16:41:07 +0400

    changes settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "postgres",
         'USER':"postgres",
-        'PASSWORD':"Baxuta777",
+        'PASSWORD':"postgres123",
         'HOST': "localhost",
         'PORT': 5432,
         
